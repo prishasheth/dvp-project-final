8f015c3256153490a657334a363647828f9da106
Sheth, Prisha Bhavin <psheth32@gatech.edu>
2024-12-04 00:31:32 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
             type: "image-and-text",
             title: "Charting the Course of Innovation: Insights into Inventor Success Factors",
             authors: ["Harita Patel, Prisha Sheth, Ajay Bhat, Saikrishnan Sankar "],
-            imageSrc: innovationImage, // Replace with the correct path to your image
+            imageSrc: innovationImage, 
             altText: "An image showing global forest decline",
         },
         {
@@ -24,19 +24,19 @@ function App() {
             type: "dashboard", 
             url: "https://public.tableau.com/views/InventorsbyState/InventorsByStateandCZ?:language=en-US&publish=yes&:origin=viz_share_link",
             title: "InventorsByStateandCZ", 
-            description: "To better understand innovation rates across various factors, let’s first begin by observing the total number of inventors by state in the US. Click on any state to delve deeper into how inventors are spread across different commuting zone*. As we can see, California has the highest number of inventors in the US, with 3956 inventors, followed by New York which has a significantly lower count of 2166.",
+            description: "To better understand innovation rates across various factors, let's first begin by observing the total number of inventors by state in the US. Click on any state to delve deeper into how inventors are spread across different commuting zone*. As we can see, <strong>California</strong> has the highest number of inventors in the US, with 3956 inventors, followed by <strong>New York</strong> which has a significantly lower count of 2166.",
             width: "100%", 
             height: "520px", 
         },
         {
             type: "paragraph",
-            content: "Let’s explore inventor counts by comparing the number of inventors across regions and genders.",
+            content: "Let's explore inventor counts by comparing the number of inventors across regions and genders.",
         },
         {
           type: "dashboard",
           url: "https://public.tableau.com/shared/RHJXFPG82?:display_count=n&:origin=viz_share_link",
           title: "Count by Gender",
-          description: "In all regions, the number of female inventors is noticeably lower than that of male inventors. This trend is consistent at the state level, where female inventor counts remain significantly lower than their male counterparts across every state. Click on any region or measure to explore the data and uncover specific insights.",
+          description: "In all regions, the <strong>number of female inventors is noticeably lower than that of male inventors</strong>. This trend is consistent at the state level, where female inventor counts remain significantly lower than their male counterparts across every state. Click on any region or measure to explore the data and uncover specific insights.",
           width: "100%",
           height: "800px",
         },
@@ -48,31 +48,31 @@ function App() {
           type: "dashboardfull",
           url: "https://public.tableau.com/views/GenderData_17331650303770/Dashboard1?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
           title: "Category by Gender",
-          description: "Comparing the number of inventors across categories reveals that the Computers and Communications category has the highest total, with the Northeast region contributing 2,712 inventors to this category. Among the seven categories, the Drugs and Medical category stands out for having the highest proportion of female inventors. Use the highlight table to filter data for the pie charts by clicking on any cell, row, or column. This allows you to compare the number of inventors by gender based on your selection on the table.",
+          description: "Comparing the number of inventors across categories reveals that the <strong>Computers and Communications category</strong> has the highest total, with the <strong>Northeast</strong> region contributing 2,712 inventors to this category. Among the seven categories, the <strong>Drugs and Medical category</strong> stands out for having the <strong>highest proportion of female inventors</strong>. Use the highlight table to filter data for the pie charts by clicking on any cell, row, or column. This allows you to compare the number of inventors by gender based on your selection on the table.",
           width: "100%", 
           height: "800px",
         },
         {
             type: "paragraph",
-            content: "Now, let’s learn about how patent applicant counts compare to patent grantee counts across the United States.",
+            content: "Now, let's learn about how patent applicant counts compare to patent grantee counts across the United States.",
         },
         {
           type: "dashboard",
           url: "https://public.tableau.com/shared/8F7W3HSJ2?:display_count=n&:origin=viz_share_link",
           title: "Applicant to Grantee Ratio",
-          description: "States with a higher applicant-to-grantee ratio indicate a larger gap between patent applicants and grantees, suggesting that in those states, a smaller proportion of applicants are granted patents. Washington D.C. has the highest applicant-to-grantee ratio of 1.47, indicating that for every grantee, there are 1.47 applicants, suggesting a relatively larger gap between applicants and those granted patents in the region.",
+          description: "States with a higher applicant-to-grantee ratio indicate a larger gap between patent applicants and grantees, suggesting that in those states, a smaller proportion of applicants are granted patents. <strong>Washington D.C.</strong> has the highest applicant-to-grantee ratio of 1.47, indicating that for every grantee, there are 1.47 applicants, suggesting a relatively larger gap between applicants and those granted patents in the region.",
           width: "100%", 
           height: "800px",
         },
         {
             type: "paragraph",
-            content: "Let’s now compare the patent applicant and grantee counts across age groups.",
+            content: "Let's now compare the patent applicant and grantee counts across age groups.",
         },
         {
           type: "dashboard",
           url: "https://public.tableau.com/shared/M5H93WGST?:display_count=n&:origin=viz_share_link",
           title: "Comparison of Applicant Count and Grantee Count By Age",
-          description: "We observe a bell curve pattern in both applicant and grantee counts as age increases. The gap between applicant and grantee counts widens until it peaks around age 41, after which the gap begins to decrease.",
+          description: "We observe a <strong>bell curve pattern</strong> in both applicant and grantee counts as age increases. The gap between applicant and grantee counts widens until it peaks around age 41, after which the gap begins to decrease.",
           width: "100%", 
         },
         {
@@ -83,7 +83,7 @@ function App() {
           type: "dashboard",
           url: "https://public.tableau.com/views/DVP_Table2/NewYearSliderFinal?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
           title: "Comparison of Applicant Count and Grantee Count By Age",
-          description: "Over the years, we observe that the gap between applicant and grantee counts is widening, indicating that a smaller proportion of applicants are being granted patents.",
+          description: "Over the years, we observe that the <strong>gap between applicant and grantee counts is widening</strong>, indicating that a smaller proportion of applicants are being granted patents.",
           width: "100%", 
         },
         {
@@ -94,28 +94,28 @@ function App() {
           type: "dashboard",
           url: "https://public.tableau.com/shared/5Y6YHX9S7?:display_count=n&:origin=viz_share_link",
           title: "Dashboard 2",
-          description: "For each gender, we can observe the counts of patent applicants and grantees. As with earlier observations, female patent applicants and grantees are significantly lower than their male counterparts. Also, we can observe that the gap between applicants and grantees follows a similar pattern to earlier as well. As age increases, the gap between applicants and grantees widens until it peaks, then the gap begins to decrease.",
+          description: "For each gender, we can observe the counts of patent applicants and grantees. As with earlier observations, <strong>female patent applicants and grantees are significantly lower than their male counterparts</strong>. Also, we can observe that the gap between applicants and grantees follows a similar pattern to earlier as well. As age increases, the gap between applicants and grantees widens until it peaks, then the gap begins to decrease.",
           width: "100%",
         },
         {
             type: "paragraph",
-            content: "Now, let’s shift our attention to the number of patents produced by different regions across the United States.",
+            content: "Now, let's shift our attention to the number of patents produced by different regions across the United States.",
         },
         {
             type: "image",
-            description: "This map distorts the size of regions based on the number of patents they produce, making areas with more patents appear larger and those with fewer appear smaller.  A clear regional pattern is evident, with states in the West, Northeast, and Midwest region producing the most patents, while states in the Southeast and Southwest contribute fewer patents. This highlights the uneven distribution of patent production across the country.",
+            description: "This map distorts the size of regions based on the number of patents they produce, making areas with more patents appear larger and those with fewer appear smaller. A clear regional pattern is evident, with states in the <strong>West, Northeast, and Midwest region producing the most patents</strong>, while states in the Southeast and Southwest contribute fewer patents. This highlights the uneven distribution of patent production across the country.",
             src: cartogramImage,
             width: "100%",
         }, 
         {
             type: "paragraph",
-            content: "Next, let’s take a closer look into individual states to examine their contribution to the total number of patents.",
+            content: "Next, let's take a closer look into individual states to examine their contribution to the total number of patents.",
         },
         {
           type: "dashboardfull",
           url: "https://public.tableau.com/views/CS6730/ParetoChart?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
           title: "Dashboard 2",
-          description: "By examining the bars for each state and the cumulative line, we can see how a few states account for the majority of the patents, with the remaining states contributing far less. California leads with a notably higher patent count than the other states, highlighting its dominant role in patent production.",
+          description: "By examining the bars for each state and the cumulative line, we can see how a few states account for the majority of the patents, with the remaining states contributing far less. <strong>California leads with a notably higher patent count</strong> than the other states, highlighting its dominant role in patent production.",
           width: "100%", 
         },
         {
@@ -137,18 +137,18 @@ function App() {
           type: "dashboardfull",
           url: "https://public.tableau.com/views/CS6730/PatentShareonParentalIncome?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
           title: "Dashboard 2",
-          description: "Examine the proportion of inventors in each parental income quintile* in each state. Each bar represents a state, with colors showing patent ownership by parental income quintile groups. Individuals from the highest parental income group consistently make up 25-30% of inventors across states. In states like Massachusetts, this disparity is even more pronounced compared to lower income groups. These patterns highlight a notable gap in inventor representation across parental income levels nationwide.",
+          description: "Examine the proportion of inventors in each parental income quintile* in each state. Each bar represents a state, with colors showing patent ownership by parental income quintile groups. Individuals from the highest parental income group consistently make up 25-30% of inventors across states. In areas like <strong>Washington D.C.</strong> and <strong>Massachusetts</strong>, this disparity is even more pronounced compared to lower income groups. These patterns highlight a notable <strong>gap in inventor representation across parental income levels</strong> nationwide.",
           width: "100%"
         },
         {
             type: "paragraph",
-            content: "Finally, let’s look at how the median and mean incomes of inventors have evolved over time.",
+            content: "Finally, let's look at how the median and mean incomes of inventors have evolved over time.",
         },
         {
             type: "dashboard",
             url: "https://public.tableau.com/views/Book3_17331659142060/MeanAndAverageIncome?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
             title: "Dashboard 2",
-            description: "The median income remains relatively steady, while the mean income fluctuates significantly. The disparity between these two lines highlights how income distribution among inventors varies, with the mean income being more affected by high earners, while the median reflects a more stable earning pattern",
+            description: "The <strong>median income remains relatively steady</strong>, while the <strong>mean income fluctuates significantly</strong>. The disparity between these two lines highlights how income distribution among inventors varies, with the mean income being more affected by high earners, while the median reflects a more stable earning pattern",
             width: "100%", 
           },
         {
@@ -159,7 +159,6 @@ function App() {
 
     return (
         <>
-            {/* Render the image-and-text section outside the app-container to span full width */}
             {sections.map((section, index) => {
                 if (section.type === "image-and-text") {
                     return (
@@ -179,10 +178,9 @@ function App() {
                         </div>
                     );
                 }
-                return null; // Ignore other types for this pass
+                return null; 
             })}
     
-            {/* Render the rest of the sections inside the app-container */}
             <div className="app-container">
                 {sections.map((section, index) => {
                     if (section.type === "paragraph") {
@@ -199,7 +197,7 @@ function App() {
                             <div key={index} className="section">
                                 <div className="dashboard-section flipped">
                                     <div className="dashboard-text">
-                                        <p>{section.description}</p>
+                                        <p dangerouslySetInnerHTML={{ __html: section.description }} />
                                     </div>
                                     <div className="dashboard-container">
                                         <TableauEmbed
@@ -224,7 +222,7 @@ function App() {
                                         />
                                     </div>
                                     <div className="dashboardfull-text">
-                                        <p>{section.description}</p>
+                                        <p dangerouslySetInnerHTML={{ __html: section.description }} /> 
                                     </div>
                                 </div>
                             </div>
